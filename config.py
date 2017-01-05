a152eb1008bc0cb19b6dfbb43d9a5ba784b0b2a3
fananran <fananran@kzx-ww-33044.citiccardins.com>
2017-01-05 21:24:44 +0800

    commit

--- config.py
+++ config.py
@@ -2,32 +2,27 @@
 from xml.dom.minidom import parse
 import xml.dom.minidom
 import httplib,urllib
+import json
 
-DOMTree = xml.dom.minidom.parse("movies.xml")
-collection = DOMTree.documentElement
-if collection.hasAttribute("shelf"):
-   print "Root element : %s" % collection.getAttribute("shelf")
-movies = collection.getElementsByTagName("movie")
-for movie in movies:
-   print "*****Movie*****"
-   if movie.hasAttribute("title"):
-      print "Title: %s" % movie.getAttribute("title")
-   type = movie.getElementsByTagName('type')[0]
-   print "Type: %s" % type.childNodes[0].data
-   format = movie.getElementsByTagName('format')[0]
-   print "Format: %s" % format.childNodes[0].data
-   rating = movie.getElementsByTagName('rating')[0]
-   print "Rating: %s" % rating.childNodes[0].data
-   description = movie.getElementsByTagName('description')[0]
-   print "Description: %s" % description.childNodes[0].data
-conn = httplib.HTTPConnection("www.baidu.com")
-conn.request("GET","/")
-res=conn.getresponse()
-print res.status, res.reason
-data=res.read()
-print(len(data))
-
-def postData(json ,url,port)
+def xmlParse(file):
+	DOMTree = xml.dom.minidom.parse(file)
+	root = DOMTree.documentElement
+	rows=root.getElementsByTagName("ROW")
+	for row in rows:
+		groupId=row.getElementsByTagName("GROUP_ID")[0].childNodes[0].data
+		tagName=row.getElementsByTagName("TAG_NAME")[0].childNodes[0].data
+		appId=row.getElementsByTagName("APP_ID")[0].childNodes[0].data
+		serviceId=row.getElementsByTagName("SERVICE_ID")[0].childNodes[0].data
+		reqClass=row.getElementsByTagName("REQUEST_CLASS")[0].childNodes[0].data
+		reqMethod =row.getElementsByTagName("REQUEST_METHOD")[0].childNodes[0].data
+		respClass=row.getElementsByTagName("RESPONSE_CLASS")[0].childNodes[0].data
+		respMethod=row.getElementsByTagName("RESPONSE_METHOD")[0].childNodes[0].data
+		objstr=json.dumps({"groupId":groupId,"tagName":tagName,"appId":appId,"serviceId":serviceId,"reqClass":reqClass,"reqMethod":reqMethod,"respClass":respClass,"respMethod":respMethod})
+		print objstr
+		postData(objstr,localhost,9200)
+		obj=json.loads(objstr)
+		print obj["tagName"]		
+def postData(json,url,port):
 	try:
 		urllib.urlencode(json)
 		headers = {"Content-type":
@@ -40,16 +35,12 @@ def postData(json ,url,port)
 		print response.reason
 		print response.read()
 		print response.getheaders()
-	except Exception e:
-		print e
+	except Exception:
+		print "error"
 	finally:
 		if httpClient:
 			httpClient.close()
-def xmlParse(file)
-	DOMTree = xml.dom.minidom.parse(file)
-	collection = DOMTree.documentElement
-	row=collection.getElementsByTagName("DATA");
-	
-
+if __name__ == '__main__':
+	xmlParse("data_total.xml")		
 
 	
